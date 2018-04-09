fbe6d91a8a6a0ce19775142b9121223755c6e9f8
krishan <krishan14sharma@gmail.com>
2018-04-09 12:16:21 +0530

    fix

--- app/src/main/java/kr/ifttutilities/bleMiClient/MiBandProtocol.kt
+++ app/src/main/java/kr/ifttutilities/bleMiClient/MiBandProtocol.kt
@@ -8,5 +8,5 @@ import java.util.*
 
 val alertMessageServiceUUID = UUID.fromString("00001811-0000-1000-8000-00805f9b34fb")
 val newAlertCharacteristicUUID = UUID.fromString("00002a46-0000-1000-8000-00805f9b34fb")
-val sendMessageAlertProtodcolHeader = byteArrayOf(0x05, 0x01) // 1. type of message 2. unread counts
+val sendMessageAlertProtocolHeader = byteArrayOf(0x05, 0x01) // 1. type of message 2. unread counts
 val BAND_NAME = "Mi Band"
